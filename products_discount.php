4d19c57c34bd3701bf9610a1456253c7f7ef569b
Shohan Islam Joy <106506181+Shohan999Ronol@users.noreply.github.com>
2023-09-03 08:16:16 +0600

    title deleted.

--- products_discount.php
+++ products_discount.php
@@ -170,7 +170,7 @@ include('login_check.php');
 					<div class="container-fluid">
 						<div class="row mb-2">
 							<div class="col-sm-6">
-								<h1>Products Discount</h1>
+								
 							</div>
 							
 						</div>
